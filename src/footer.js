3c4e2f44059b2a0a7b5bf7ee4eb8b8f69aa6bfc2
Joseph Gilgen <gilgenlabs@gmail.com>
2018-05-17 11:29:56 -0700

    updated contact us

--- src/footer.js
+++ src/footer.js
@@ -1,3 +1,5 @@
 import React from "react"
 
-export default () => <div>© Gilgen Labs 2018</div>
+export default () => (
+  <div style={{ padding: 20, textAlign: "center" }}>© Gilgen Labs 2018</div>
+)
